fdca235547f51a1ff483602f6c897005db79f32c
Donald Wu <yeukfei02@gmail.com>
2021-01-06 19:16:47 +0800

    feat: 🎸 add context in handler

--- src/graphql/handler.ts
+++ src/graphql/handler.ts
@@ -2,8 +2,8 @@
 // env.config();
 
 import { ApolloServer } from 'apollo-server-lambda';
-import typeDefs from '../schema/typeDefs';
-import resolvers from '../resolvers/resolvers';
+import typeDefs from '../../schema/typeDefs';
+import resolvers from '../../resolvers/resolvers';
 
 import awsXRay from 'aws-xray-sdk';
 import awsSdk from 'aws-sdk';
@@ -15,7 +15,14 @@ const server = new ApolloServer({
   tracing: true,
   introspection: true,
   playground: {
-    endpoint: '/prod',
+    endpoint: '/dev',
+  },
+  context: ({ event, context }) => {
+    const data = {
+      event: event,
+      context: context,
+    };
+    return data;
   },
 });
 
