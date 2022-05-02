1eebb6570e2f7d74b29140449760ffe9326ed9fc
Donald Wu <yeukfei02@gmail.com>
2022-05-03 02:27:45 +0800

    fix: 🐛 fix busRoute and busService logic

--- request/allBusRoute.ts
+++ request/allBusRoute.ts
@@ -1,79 +1,24 @@
 // import env from 'dotenv';
 // env.config();
 
-import axios from 'axios';
-import _ from 'lodash';
+import SingaporeBusRoute from '../model/SingaporeBusRoute';
 
-import { BusRoute, Value } from '../interface/BusRoute';
+export const getAllBusRoute = async (busServiceNo?: string): Promise<any> => {
+  let singaporeBusRoute: any = null;
 
-export const getAllBusRoute = async (): Promise<any> => {
-  const busRouteList: any[] = [];
-
-  let skipNum = 0;
-  let loopStatus = true;
-  while (loopStatus) {
-    const responseDataValue = await fetchBusRoute(skipNum);
-    if (!_.isEmpty(responseDataValue)) {
-      for (let index = 0; index < responseDataValue.length; index++) {
-        const item = responseDataValue[index];
-
-        const obj = {
-          serviceNo: item.ServiceNo,
-          operator: item.Operator,
-          direction: item.Direction,
-          stopSequence: item.StopSequence,
-          busStopCode: item.BusStopCode,
-          distance: item.Distance,
-          wdFirstBus: item.WD_FirstBus,
-          wdLastBus: item.WD_LastBus,
-          satFirstBus: item.SAT_FirstBus,
-          satLastBus: item.SAT_LastBus,
-          sunFirstBus: item.SUN_FirstBus,
-          sunLastBus: item.SUN_LastBus,
-        };
-        busRouteList.push(obj);
-      }
-
-      skipNum += 500;
-    } else {
-      loopStatus = false;
-    }
-  }
-
-  return busRouteList;
-};
-
-async function fetchBusRoute(skipNum?: number) {
-  let valueList: Value[] = [];
-
-  let response: any = null;
-  if (skipNum === 0) {
-    response = await axios.get(`http://datamall2.mytransport.sg/ltaodataservice/BusRoutes`, {
-      headers: {
-        AccountKey: process.env.ACCOUNT_KEY,
-        Accept: 'application/json',
-      },
-    });
+  if (!busServiceNo) {
+    singaporeBusRoute = await SingaporeBusRoute.scan().all().exec();
   } else {
-    response = await axios.get(`http://datamall2.mytransport.sg/ltaodataservice/BusRoutes`, {
-      params: {
-        $skip: skipNum,
-      },
-      headers: {
-        AccountKey: process.env.ACCOUNT_KEY,
-        Accept: 'application/json',
-      },
-    });
+    singaporeBusRoute = await SingaporeBusRoute.query({ serviceNo: { eq: busServiceNo } })
+      .using('serviceNoIndex')
+      .all()
+      .exec();
   }
 
-  if (response) {
-    const responseData: BusRoute = response.data;
-    console.log('responseData = ', responseData);
-
-    if (responseData && responseData.value) {
-      valueList = responseData.value;
-    }
+  let singaporeBusRouteList = [];
+  if (singaporeBusRoute) {
+    singaporeBusRouteList = singaporeBusRoute.toJSON();
   }
 
-  return valueList;
-}
+  return singaporeBusRouteList;
+};
