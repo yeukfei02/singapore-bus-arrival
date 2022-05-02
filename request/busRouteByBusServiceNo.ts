988e3dc00bb474697cd06302ca44e3701b4d60d2
Donald Wu <yeukfei02@gmail.com>
2022-05-03 00:25:31 +0800

    fix: 🐛 fix getBusRouteByBusServiceNo api logic

--- request/busRouteByBusServiceNo.ts
+++ request/busRouteByBusServiceNo.ts
@@ -1,93 +1,56 @@
 // import env from 'dotenv';
 // env.config();
 
-import axios from 'axios';
 import _ from 'lodash';
 
-import { BusRoute, Value } from '../interface/BusRoute';
+import { getAllBusRoute } from '../request/allBusRoute';
 import { getBusStopByBusStopCode } from './busStopByBusStopCode';
 
 export const getBusRouteByBusServiceNo = async (busServiceNo: string): Promise<any> => {
   const busRouteList: any[] = [];
 
-  let skipNum = 0;
-  let loopStatus = true;
-  while (loopStatus) {
-    const responseDataValue = await fetchBusRoute(skipNum);
-    if (!_.isEmpty(responseDataValue)) {
-      for (let index = 0; index < responseDataValue.length; index++) {
-        const item = responseDataValue[index];
-
-        if (item.ServiceNo === busServiceNo) {
-          let busStopCodeList = [];
-          if (item.BusStopCode) {
-            busStopCodeList = await getBusStopByBusStopCode(item.BusStopCode);
-          }
-
-          let busStop = {};
-          if (!_.isEmpty(busStopCodeList)) {
-            busStop = busStopCodeList[0];
-          }
-
-          const obj = {
-            serviceNo: item.ServiceNo,
-            operator: item.Operator,
-            direction: item.Direction,
-            stopSequence: item.StopSequence,
-            busStopCode: item.BusStopCode,
-            busStop: busStop,
-            distance: item.Distance,
-            wdFirstBus: item.WD_FirstBus,
-            wdLastBus: item.WD_LastBus,
-            satFirstBus: item.SAT_FirstBus,
-            satLastBus: item.SAT_LastBus,
-            sunFirstBus: item.SUN_FirstBus,
-            sunLastBus: item.SUN_LastBus,
-          };
-          busRouteList.push(obj);
-        }
+  const allBusRouteList = await getAllBusRoute();
+  if (!_.isEmpty(allBusRouteList)) {
+    const busRouteList = allBusRouteList.filter((item: any, i: number) => {
+      if (item.serviceNo === busServiceNo) {
+        return item;
       }
+    });
+    console.log('busRouteList = ', busRouteList);
 
-      skipNum += 500;
-    } else {
-      loopStatus = false;
+    if (!_.isEmpty(busRouteList)) {
+      for (let index = 0; index < busRouteList.length; index++) {
+        const item = busRouteList[index];
+
+        let busStopCodeList = [];
+        if (item.busStopCode) {
+          busStopCodeList = await getBusStopByBusStopCode(item.busStopCode);
+        }
+
+        let busStop = {};
+        if (!_.isEmpty(busStopCodeList)) {
+          busStop = busStopCodeList[0];
+        }
+
+        const obj = {
+          serviceNo: item.ServiceNo,
+          operator: item.Operator,
+          direction: item.Direction,
+          stopSequence: item.StopSequence,
+          busStopCode: item.BusStopCode,
+          busStop: busStop,
+          distance: item.Distance,
+          wdFirstBus: item.WD_FirstBus,
+          wdLastBus: item.WD_LastBus,
+          satFirstBus: item.SAT_FirstBus,
+          satLastBus: item.SAT_LastBus,
+          sunFirstBus: item.SUN_FirstBus,
+          sunLastBus: item.SUN_LastBus,
+        };
+        busRouteList.push(obj);
+      }
     }
   }
 
   return busRouteList;
 };
-
-async function fetchBusRoute(skipNum?: number) {
-  let valueList: Value[] = [];
-
-  let response: any = null;
-  if (skipNum === 0) {
-    response = await axios.get(`http://datamall2.mytransport.sg/ltaodataservice/BusRoutes`, {
-      headers: {
-        AccountKey: process.env.ACCOUNT_KEY,
-        Accept: 'application/json',
-      },
-    });
-  } else {
-    response = await axios.get(`http://datamall2.mytransport.sg/ltaodataservice/BusRoutes`, {
-      params: {
-        $skip: skipNum,
-      },
-      headers: {
-        AccountKey: process.env.ACCOUNT_KEY,
-        Accept: 'application/json',
-      },
-    });
-  }
-
-  if (response) {
-    const responseData: BusRoute = response.data;
-    console.log('responseData = ', responseData);
-
-    if (responseData && responseData.value) {
-      valueList = responseData.value;
-    }
-  }
-
-  return valueList;
-}
