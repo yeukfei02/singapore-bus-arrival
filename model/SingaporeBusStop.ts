990d8d486d54e99ea33ae9f5f530882c4016aecc
Donald Wu <yeukfei02@gmail.com>
2022-05-03 12:42:09 +0800

    feat: 🎸 add allBusStop cron job

---
+++ model/SingaporeBusStop.ts
@@ -0,0 +1,34 @@
+import dynamoose from 'dynamoose';
+dynamoose.aws.sdk.config.update({
+  accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+  region: 'ap-southeast-1',
+});
+
+const singaporeBusStopSchema = new dynamoose.Schema(
+  {
+    id: String,
+    busStopCode: {
+      type: String,
+      index: {
+        name: 'busStopCodeIndex',
+        global: true,
+      },
+    },
+    roadName: String,
+    description: String,
+    latitude: Number,
+    longitude: Number,
+  },
+  {
+    saveUnknown: true,
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+  },
+);
+
+const singaporeBusStopModel = dynamoose.model('SingaporeBusStop', singaporeBusStopSchema);
+
+export default singaporeBusStopModel;
---
+++ src/allBusStop/handler.ts
@@ -0,0 +1,87 @@
+import { APIGatewayEvent, Context, Handler } from 'aws-lambda';
+import axios from 'axios';
+import _ from 'lodash';
+import { v4 as uuidv4 } from 'uuid';
+import SingaporeBusStop from '../../model/SingaporeBusStop';
+import { BusStopCode, Value } from '../../interface/BusStopCode';
+
+export const allBusStop: Handler = async (event: APIGatewayEvent, context: Context) => {
+  await deleteAllBusStop();
+
+  let skipNum = 0;
+  let loopStatus = true;
+  while (loopStatus) {
+    const responseDataValue = await fetchBusStop(skipNum);
+    if (!_.isEmpty(responseDataValue)) {
+      for (let index = 0; index < responseDataValue.length; index++) {
+        const item = responseDataValue[index];
+
+        const singaporeBusStop = new SingaporeBusStop({
+          id: uuidv4(),
+          busStopCode: item.BusStopCode,
+          roadName: item.RoadName,
+          description: item.Description,
+          latitude: item.Latitude,
+          longitude: item.Longitude,
+        });
+        await singaporeBusStop.save();
+      }
+
+      skipNum += 500;
+    } else {
+      loopStatus = false;
+    }
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'allBusStop',
+    }),
+  };
+  return response;
+};
+
+async function deleteAllBusStop() {
+  const singaporeBusStop = await SingaporeBusStop.scan().all().exec();
+  const singaporeBusStopList = singaporeBusStop.toJSON();
+  for (let index = 0; index < singaporeBusStopList.length; index++) {
+    const singaporeBusStop = singaporeBusStopList[index];
+    await singaporeBusStop.delete();
+  }
+}
+
+async function fetchBusStop(skipNum?: number) {
+  let valueList: Value[] = [];
+
+  let response: any = null;
+  if (skipNum === 0) {
+    response = await axios.get(`http://datamall2.mytransport.sg/ltaodataservice/BusStops`, {
+      headers: {
+        AccountKey: process.env.ACCOUNT_KEY,
+        Accept: 'application/json',
+      },
+    });
+  } else {
+    response = await axios.get(`http://datamall2.mytransport.sg/ltaodataservice/BusStops`, {
+      params: {
+        $skip: skipNum,
+      },
+      headers: {
+        AccountKey: process.env.ACCOUNT_KEY,
+        Accept: 'application/json',
+      },
+    });
+  }
+
+  if (response) {
+    const responseData: BusStopCode = response.data;
+    console.log('responseData = ', responseData);
+
+    if (responseData && responseData.value) {
+      valueList = responseData.value;
+    }
+  }
+
+  return valueList;
+}
